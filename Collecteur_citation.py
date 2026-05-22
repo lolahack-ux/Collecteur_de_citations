16cc1016ab83bfe8c9a6028e224d2b3d46acd74a
Lola Erhart <erhart.lola@hotmail.fr>
2026-05-22 13:54:48 +0200

    <feat>Generation_html

--- Collecteur_citation.py
+++ Collecteur_citation.py
@@ -1,9 +1,41 @@
 import requests
+url="http://api.quotable.io/random"
 
-response = requests.get("http://api.quotable.io/random")
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print("Erreur :", response.status_code)
+#LOOP
+i = 0
+citations = [] 
+
+for i in range (0, 5):
+    connexion = requests.get(url)
+    donnees = connexion.json()
+    citation = {
+    "texte" : donnees['content'],
+    "auteur" : donnees['author']
+    } 
+    citations.append(citation)
+
+with open("citations.html", "w", encoding="utf-8") as f:
+
+    f.write("<!DOCTYPE html>\n")
+    f.write("<html lang='fr'>\n")
+
+    f.write("<head>\n")
+    f.write("<meta charset='UTF-8'>\n")
+    f.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n")
+    f.write("<title>Citations</title>\n")
+    f.write("<link rel='stylesheet' href='style.css'>")
+    f.write("</head>\n")
+
+    f.write("<body>\n")
+
+    for i in range(len(citations)):
+        f.write("  <div class='item'>\n")
+        f.write(f"    <p>{citations[i]["texte"]}</p>\n")
+        f.write(f"    <p>— {citations[i]["auteur"]}</p>\n")
+        f.write("  </div>\n\n")
+    f.write("</body>\n")
+    f.write("</html>\n")
+
+
+print("5 citationns ajoutées")
